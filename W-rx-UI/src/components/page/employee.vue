dc03aa636ac69721ef0ea78ada7eef51f07fdbfa
王荣星 <1679400070@qq.com>
2025-11-05 14:21:02 +0800

    新增员工弹出框验证

--- W-rx-UI/src/components/page/employee.vue
+++ W-rx-UI/src/components/page/employee.vue
@@ -134,6 +134,17 @@ export default {
       }
     };
     
+    // 密码验证规则 - 必须包含字母和数字
+    const validatePassword = (rule, value, callback) => {
+      // 正则表达式：至少包含一个字母和一个数字
+      const reg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,20}$/;
+      if (!reg.test(value)) {
+        callback(new Error('密码必须包含字母和数字，长度6-20位'));
+      } else {
+        callback();
+      }
+    };
+    
     // 手机号验证规则
     const validatePhone = (rule, value, callback) => {
       const reg = /^1[3-9]\d{9}$/;
@@ -201,13 +212,16 @@ export default {
             trigger: 'blur'
           },
           {
-            min: 6,
-            max: 20,
-            message: '密码长度应在6-20个字符之间',
+            validator: validatePassword,
             trigger: 'blur'
           }
         ],
         phone: [
+          {
+            required: true,
+            message: '请输入手机号',
+            trigger: 'blur'
+          },
           {
             validator: validatePhone,
             trigger: 'blur'
